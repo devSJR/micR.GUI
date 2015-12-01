4a8f9eb1473cfdce55128dc855f75b3bb2b3a167
devSJR <stefan.roediger@b-tu.de>
2015-12-01 13:33:31 +0100

    some further functionality

--- micR.GUI_rk.R
+++ micR.GUI_rk.R
@@ -25,13 +25,25 @@ local({
   
   # Define dependencies
   dependencies.info <- rk.XML.dependencies(dependencies = list(rkward.min = "0.6.3"), 
-                                           package = list(c(name = "micR", min = "0.0.1")))
+					   package = list(c(name = "doParallel", min = "1.0.10"),
+							  c(name = "EBImage", min = "4.12.0"),
+							  c(name = "foreach", min = "1.4.3"),
+							  c(name = "micR", min = "0.0.1")
+							 ))
   ## General settings
+  # File browser for XLS data.
+  dye.DAPI <- rk.XML.browser("DAPI:", url = TRUE, required = TRUE, filter = c("*.bmp", "*.png"))
+  dye.FITC <- rk.XML.browser("FITC:", url = TRUE, required = FALSE, filter = c("*.bmp", "*.png"))
+  dye.Cy3 <- rk.XML.browser("Cy3:", url = TRUE, required = FALSE, filter = c("*.bmp", "*.png"))
+  dye.APC <- rk.XML.browser("APC:", url = TRUE, required = FALSE, filter = c("*.bmp", "*.png"))
   
   preview.chk <- rk.XML.preview(label = "Preview")
   
   basic.settings <- rk.XML.row(
-    rk.XML.col(
+				rk.XML.col(	dye.DAPI,
+						dye.FITC,
+						dye.Cy3,
+						dye.APC,
      		 preview.chk,
       rk.XML.stretch()
     ))
@@ -45,14 +57,19 @@ local({
   )
   
   JS.calc <- rk.paste.JS(
-			  echo("img.APC  <- readImage(system.file('images/Well_Slide2_9_APC.png', package='micR'))\n"),
+			  echo("img.DAPI  <- readImage(\"", dye.DAPI,"\")\n"),
+			  echo("img.FITC  <- readImage(\"", dye.FITC,"\")\n"),
+			  echo("img.Cy3  <- readImage(\"", dye.Cy3,"\")\n"),
+			  echo("img.APC  <- readImage(\"", dye.APC,"\")\n"),
 			  echo("img.pp   <- img.processor(img.raw = img.DAPI)\n"),
 			  echo("img.xy	 <- computeFeatures(img.pp, img.DAPI, xname = \"nucleus\")\n"),
 			  echo("img.moment <- computeFeatures.moment(img.pp)\n"),
-			    echo("par(mfrow = c(1,2))\n"),
-  echo("display(img.DAPI, method = \"raster\")\n"),
-  echo("display(img.pp, method = \"raster\")\n")
-  )
+			  echo("numWorkers <- detectCores()\n"),
+			  echo("cl <- makeCluster(numWorkers, type = \"PSOCK\")\n"),
+			  echo("registerDoParallel(cl)\n"),
+			  echo("list.data <- list(DAPI = img.DAPI, Cy3 = img.Cy3, APC = img.APC, FITC = img.FITC)\n"),
+			  echo("res.out <- foreach(i = 1L:length(list.data), .packages = \"micR\") %dopar% spott(img.raw = list.data[[i]], img.pp = img.pp, img.moment  = img.moment, quantile = 0.03)\n")
+			)
   
   JS.print <- rk.paste.JS(
   echo("par(mfrow = c(1,2))\n"),
@@ -65,7 +82,7 @@ local({
     about = about.info,
     dependencies = dependencies.info,
     xml = list(dialog = full.dialog),
-    js = list(require = "micR",
+    js = list(require = c("doParallel", "EBImage", "foreach", "micR"),
               calculate = JS.calc,
               doPrintout = JS.print,
               results.header = FALSE), # results.header = FALSE is used for backward compatibility (RKWard v. < 0.6.2). Shoudl be removed for RKWard 0.6.3 or later
